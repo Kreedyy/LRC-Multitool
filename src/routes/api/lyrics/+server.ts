d26d3e462a3109c47262fdb49c6016841eeb7c9e
Kreedyy <kreedyhaj@gmail.com>
2026-02-03 03:13:50 +0100

    Test GET

--- src/routes/api/lyrics/+server.ts
+++ src/routes/api/lyrics/+server.ts
@@ -1,1 +1,11 @@
 //Add rate limiting and sanitization for submitting to LRCLIB
+import { json } from '@sveltejs/kit';
+
+export async function GET({ url, fetch }) {
+  const q = url.searchParams.get('q');
+  
+  const response = await fetch(`https://api.example.com/search?q=${q}`);
+  const data = await response.json();
+  
+  return json(data);
+}
--- src/routes/lyrics/+page.svelte
+++ src/routes/lyrics/+page.svelte
@@ -1,7 +1,16 @@
 <script>
 	import LyricBox from "$lib/assets/LyricBox.svelte";
 	import LyricSearch from "$lib/assets/LyricSearch.svelte";
-
+	import { onMount } from "svelte";
+  async function search() {
+    const response = await fetch('/api/lyrics?q=i hate bipolar girls');
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  onMount(() => {
+    search();
+  });
 </script>
 
 <div>
