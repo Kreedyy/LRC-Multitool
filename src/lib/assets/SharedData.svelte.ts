13c6bfad12b095bf6a507dd066064376bc8d3954
Kreedyy <kreedyhaj@gmail.com>
2026-02-04 03:27:06 +0100

    Implement setTrackData for submitting

--- src/lib/assets/SharedData.svelte.ts
+++ src/lib/assets/SharedData.svelte.ts
@@ -5,3 +5,43 @@ export let shared = $state<{
 	duration?: number;
 	lyrics?: string;
 }>({});
+
+export function setTrackData({
+  artist,
+  track,
+  album,
+  duration,
+  lyrics
+}: {
+  artist?: string;
+  track?: string;
+  album?: string;
+  duration?: number;
+  lyrics?: string;
+} = {}) {
+  artist && setArtist(artist);
+  track && setTrack(track);
+  album && setAlbum(album);
+  duration && setDuration(duration);
+  lyrics && setLyrics(lyrics);
+}
+
+export function setArtist(artist: string){
+	shared.artist = artist;
+}
+
+export function setTrack(track: string) {
+	shared.track = track;
+}
+
+export function setAlbum(album: string) {
+	shared.album = album;
+}
+
+export function setDuration(duration: number) {
+	shared.duration = duration;
+}
+
+export function setLyrics(lyrics: string) {
+	shared.lyrics = lyrics;
+}
